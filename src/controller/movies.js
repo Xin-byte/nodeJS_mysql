62bcadcec6ba11b2d8b4c8ca2886f27b22b0eb28
Xin-byte <torresfranky30@gmail.com>
2023-12-30 21:20:01 -0500

    added get movies by genre by Fran Torres

--- src/controller/movies.js
+++ src/controller/movies.js
@@ -2,7 +2,7 @@ import { MovieModel } from '../model/movies.js'
 
 export class MovieController {
   static async getAll (req, res) {
-    const { genre } = req.body
+    const { genre } = req.query
     const movies = await MovieModel.getAll({ genre })
 
     res.json(movies)
--- src/model/movies.js
+++ src/model/movies.js
@@ -12,7 +12,26 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static async getAll ({ genre }) {
-    const result = await connection.query(
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase()
+
+      const [genres] = await connection.query(
+        'SELECT * FROM genre WHERE LOWER(name) = ?', [lowerCaseGenre]
+      )
+
+      if (genres.length === 0) return { error: 'Movies not found' }
+
+      const [{ id }] = genres
+
+      const [moviesByGenre] = await connection.query(
+        `SELECT title, year, director, poster, rate, BIN_TO_UUID(id) id FROM movie m
+         JOIN movie_genres mg ON mg.movie_id = m.id AND mg.genre_id = ?`, [id]
+      )
+
+      return moviesByGenre
+    }
+
+    const [result] = await connection.query(
       'SELECT title, year, director, poster, rate, BIN_TO_UUID(id) id FROM movie;'
     )
 
